6a363f3e21f4b3caa2443d2d5d259dc8be8758af
Cannicide <41762964+Cannicide@users.noreply.github.com>
2024-03-15 00:06:56 -0400

    Documented EventManager

--- src/manager.ts
+++ src/manager.ts
@@ -10,9 +10,16 @@ interface BrynjolfEvents extends ClientEvents {
     "*": [eventName: keyof ClientEvents, data: any[]];
 }
 
+/**
+ * A custom event emitter synchronized with Discord.js events. 
+ * You should not need to use this class directly; instead, use
+ * the {@link Members.events | events} member.
+ */
 class EventManager extends EventEmitter {
+    /** @internal Internally used to store Client. */
     private _client?: Client;
 
+    /** Sets the Discord.js `Client`, activating all event listeners. */
     public client(client: Client) {
         this._client = client;
 
@@ -29,20 +36,55 @@ class EventManager extends EventEmitter {
         Reflect.set(this._client, "_brynjolf_patched", true);
     }
 
+    /** 
+     * Emits a Discord.js or Brynjolf event.
+     * 
+     * @example
+     * events.emit("messageCreate", message);
+     */
     public override emit<Event extends keyof BrynjolfEvents>(eventName: Event, ...args: BrynjolfEvents[Event]): boolean {
         return super.emit<Event>(eventName, ...args);
     }
 
+    /**
+     * Listens to a Discord.js or Brynjolf event.
+     * 
+     * @example
+     * events.on("messageCreate", (message) => {
+     *  console.log("Message was sent!");
+     * });
+     */
     public override on<Event extends keyof BrynjolfEvents>(eventName: Event, listener: (...args: BrynjolfEvents[Event]) => void): this {
         super.on(eventName, listener);
         return this;
     }
 
+    /**
+     * Listens to a Discord.js or Brynjolf event, but only **once**.
+     * 
+     * @example
+     * events.once("messageCreate", (message) => {
+     *  console.log("Message was sent!");
+     *  console.log("Now this listener will be removed!");
+     * });
+     */
     public override once<Event extends keyof BrynjolfEvents>(eventName: Event, listener: (...args: BrynjolfEvents[Event]) => void): this {
         super.once(eventName, listener);
         return this;
     }
 
+    /**
+     * Removes a Discord.js or Brynjolf event listener.
+     * 
+     * @example
+     * const callback = (message) => {
+     *  console.log("Message was sent!");
+     * });
+     * 
+     * events.on("messageCreate", callback);
+     * // ...
+     * events.off("messageCreate", callback);
+     */
     public override off<Event extends keyof BrynjolfEvents>(eventName: Event, listener: (...args: BrynjolfEvents[Event]) => void): this {
         super.off(eventName, listener);
         return this;
